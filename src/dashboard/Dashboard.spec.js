e7cc1b255dc2d60d8dadb539d2f3131a1032454c
Cameron Alvarado <cameronalvarado@Camerons-Macintosh.local>
2019-09-16 14:31:34 -0700

    got first test to work

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,13 @@
 // Test away
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Dashboard from './Dashboard';
+
+test('Dashboard renders correctly', () => {
+  render(<Dashboard />);
+});
+
+// test('Gate should initially be unlocked', () => {
+//     expect(Dashboard.state.locked).toEqual(false)
+//   });
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,13 @@
 // Test away!
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Display from './Display';
+
+test('Display renders correctly', () => {
+  render(<Display />);
+});
+
+test('Gate should initially be unlocked', () => {
+    expect(Display.defaultProps.locked).toEqual(false)
+  });
