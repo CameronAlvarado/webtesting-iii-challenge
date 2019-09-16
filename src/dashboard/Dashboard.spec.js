5dc9797fc8f7b97a3c054bfa27564bdaa6cbd447
Cameron Alvarado <cameronalvarado@Camerons-Macintosh.local>
2019-09-16 15:57:54 -0700

    some tests done

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,13 +1,22 @@
 // Test away
 import React from 'react';
 import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'
+// import { toContain } from '@testing-library/jest-dom';
 
-import Dashboard from './Dashboard';
+// import Dashboard from './Dashboard';
+import Display from '../display/Display.js';
+import Controls from '../controls/Controls.js';
 
-test('Dashboard renders correctly', () => {
-  render(<Dashboard />);
+
+// test('Dashboard renders correctly', () => {
+//   render(<Dashboard />);
+// });
+
+test('shows Controls', () => {
+    expect(render(<Display />)).toMatchSnapshot();
+  });
+
+test('shows Controls', () => {
+    expect(render(<Controls />)).toMatchSnapshot();
 });
-
-// test('Gate should initially be unlocked', () => {
-//     expect(Dashboard.state.locked).toEqual(false)
-//   });
